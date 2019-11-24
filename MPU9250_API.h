df594bf895512d3ba0eed1f09560e4cb7096b944
Themis Papameletiou <themicp@gmail.com>
2019-11-24 02:39:36 +0200

    API for MPU9250 IMU

---
+++ MPU9250_API.h
@@ -0,0 +1,24 @@
+#include "IMU.h"
+#include "MPU9250.h"
+
+class MPU9250_API: public IMU {
+  static MPU9250 sensor;
+
+  public:
+    static MPU9250_API& getInstance();
+    float pitch();
+    float yaw();
+    float roll();
+
+    float accelerationX();
+    float accelerationY();
+    float accelerationZ();
+
+    float gyroX();
+    float gyroY();
+    float gyroZ();
+
+  private:
+    MPU9250_API();
+    void readSensorData();
+};
