307b1567f53352cf9f0edbe102edc712b0007d8f
Themis Papameletiou <themicp@gmail.com>
2019-11-24 02:39:17 +0200

    IMU Interface

---
+++ IMU.h
@@ -0,0 +1,10 @@
+class IMU {
+  public:
+    virtual float pitch(); // degrees
+    virtual float yaw(); // degrees
+    virtual float roll(); // degrees
+
+    virtual float accelerationX(); // mg
+    virtual float accelerationY(); // mg
+    virtual float accelerationZ(); // mg
+};
