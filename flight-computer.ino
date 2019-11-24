716a5c5ab3f29fd23178b490a6123ff687e13ad9
Themis Papameletiou <themicp@gmail.com>
2019-11-24 20:09:13 +0200

    Update flight computer to send pitch, yaw and roll in serial port

--- flight-computer.ino
+++ flight-computer.ino
@@ -8,7 +8,6 @@ MPU9250_API *imuSensor;
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Reading basic values from BME280");
 
   Wire.begin();
 
@@ -18,41 +17,12 @@ void setup()
 
 void loop()
 {
-  Serial.print("Pressure (Pa): ");
-  Serial.print(pressureSensor->pressure());
-  Serial.println();
-
-  Serial.print("Altitude (m): ");
-  Serial.print(pressureSensor->altitude());
-
-  Serial.println();
-
-  Serial.print("Pitch, Yaw, Roll (deg): ");
   Serial.print(imuSensor->pitch());
-  Serial.print(", ");
+  Serial.print(",");
   Serial.print(imuSensor->yaw());
-  Serial.print(", ");
+  Serial.print(",");
   Serial.print(imuSensor->roll());
-
   Serial.println();
 
-  Serial.print("Acceleration X, Y, Z (mg): ");
-  Serial.print(imuSensor->accelerationX());
-  Serial.print(", ");
-  Serial.print(imuSensor->accelerationY());
-  Serial.print(", ");
-  Serial.print(imuSensor->accelerationZ());
-
-  Serial.println();
-
-  Serial.print("Gyroscope X, Y, Z (deg/s): ");
-  Serial.print(imuSensor->gyroX());
-  Serial.print(", ");
-  Serial.print(imuSensor->gyroY());
-  Serial.print(", ");
-  Serial.print(imuSensor->gyroZ());
-
-  Serial.println();
-
-  delay(1000);
+  delay(50);
 }
