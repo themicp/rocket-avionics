3dff28884de49c76c208d37bd6f50ab22ed43ff1
Themis Papameletiou <themicp@gmail.com>
2019-12-07 17:30:37 +0200

    Remove whitespace

--- simulator/public/js/sensor.js
+++ simulator/public/js/sensor.js
@@ -2,8 +2,7 @@ const socket = new WebSocket('ws://localhost:8080');
 
 socket.addEventListener('open', (event) => {
   console.log('Connected to WebSocket server')
-});
-
+}); 
 socket.addEventListener('message', (event) => {
   data = JSON.parse(event.data)
 
