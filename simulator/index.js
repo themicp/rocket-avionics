2c8be0b011b0485fa469ff6f49ac15e8d29556a6
Themis Papameletiou <themicp@gmail.com>
2019-12-10 00:02:39 +0200

    Simulator Server: Replay logfile passed in IMU_LOGS env

--- simulator/index.js
+++ simulator/index.js
@@ -2,18 +2,22 @@ const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const express = require('express')
 const WebSocket = require('ws');
+const fs = require('fs')
 const app = express()
 
 SERVER_PORT = 3000
 WS_PORT = 8080
 
-if (!process.env.PORT_PATH) {
-  throw new Error('Provide PORT_PATH in env.')
+if (!process.env.PORT_PATH && !process.env.IMU_LOGS) {
+  throw new Error('Provide PORT_PATH or IMU_LOGS in env.')
 }
 
-const parser = new Readline()
-const serialPort = new SerialPort(process.env.PORT_PATH, { baudRate: 9600 })
-serialPort.pipe(parser)
+let parser, serialPort
+if (process.env.PORT_PATH) {
+  parser = new Readline()
+  serialPort = new SerialPort(process.env.PORT_PATH, { baudRate: 9600 })
+  serialPort.pipe(parser)
+}
 
 app.use(express.static('public'))
 app.listen(SERVER_PORT, () => {
@@ -21,10 +25,21 @@ app.listen(SERVER_PORT, () => {
   console.log(`Setting WS Server on port ${WS_PORT}`)
 
   const wss = new WebSocket.Server({port: WS_PORT});
-  wss.on('connection', function connection(ws) {
-    parser.on('data', line => {
-      [pitch, yaw, roll] = line.split(',').map(value => parseFloat(value))
-      ws.send(JSON.stringify({pitch, yaw, roll}));
-    })
-  });
+  wss.on('connection', (ws) => {
+    if (process.env.PORT_PATH) {
+      parser.on('data', line => {
+        [pitch, yaw, roll] = line.split(',').map(value => parseFloat(value))
+        ws.send(JSON.stringify({type: 'stream', data: {pitch, yaw, roll}}));
+      })
+    } else {
+      fs.readFile(process.env.IMU_LOGS, {encoding: 'utf-8'}, async (err, data) => {
+        if (!err) {
+          ws.send(JSON.stringify({type: 'replay', data: data.split('\n')}))
+        } else {
+          ws.send(JSON.stringify({error: 'Error reading IMU logs'}))
+          ws.close()
+        }
+      })
+    }
+  })
 })
