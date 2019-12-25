9ca3f5f2f466db4d73c0dce9c47f2c5a424f42f8
Themis Papameletiou <themicp@gmail.com>
2019-12-26 00:38:53 +0200

    Telemetry: Add RF95 module and setup() method

--- libraries/Telemetry.cpp
+++ libraries/Telemetry.cpp
@@ -1,6 +1,8 @@
 #include "Telemetry.h"
 
-static Telemetry& Telemetry::getInstance() {
+RH_RF95 Telemetry::rf95(8, 3);
+
+Telemetry& Telemetry::getInstance() {
   static Telemetry instance;
   return instance;
 }
@@ -9,6 +11,12 @@ void Telemetry::send(String data) {
   if (Serial) {
     int messageLength = data.length();
     Serial.println("TELEMETRY (" + String(messageLength) + "): " + data);
+  }
+}
+
+void Telemetry::setup() {
+  if (!rf95.init()) {
+    while(1);
   }
 }
 
--- libraries/Telemetry.h
+++ libraries/Telemetry.h
@@ -1,9 +1,13 @@
 #include "Arduino.h"
+#include "SPI.h"
+#include "RH_RF95.h"
 
 class Telemetry {
   public:
     static Telemetry& getInstance();
     void send(String data);
+    void setup();
   private:
+    static RH_RF95 rf95;
     Telemetry();
 };
