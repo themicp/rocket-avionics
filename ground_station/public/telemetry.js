7591be19d8a840d8ed08b098813161953ba33ce9
Themis Papameletiou <themicp@gmail.com>
2019-12-26 20:22:10 +0200

    Add generic telemetry adaptor in JS

---
+++ ground_station/public/telemetry.js
@@ -0,0 +1,31 @@
+const delay = async (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+class Telemetry extends EventTarget {
+  constructor(...args) {
+    super(...args)
+
+    this.socket = new WebSocket('ws://localhost:8080');
+
+    this.socket.addEventListener('open', (event) => {
+      console.log('Connected to WebSocket server')
+    })
+
+    this.socket.addEventListener('message', ({data}) => this.dispatchEvent(new CustomEvent('data', {detail: data})))
+  }
+
+  sendCalibrate() {
+    this.socket.send('COMMAND:init_calibration')
+  }
+
+  sendLaunched() {
+    this.socket.send('COMMAND:launched')
+  }
+
+  sendFTS() {
+    this.socket.send('COMMAND:trigger_fts')
+  }
+}
