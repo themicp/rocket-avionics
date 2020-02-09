daf6bb33f2d7c088a8c74972267606c2ba14b41c
Themis Papameletiou <themicp@gmail.com>
2020-02-09 02:15:35 +0200

    Move DB logging to external script

---
+++ ground_station/db-logger.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : '',
+  password : '',
+  database : ''
+});
+
+connection.connect();
+
+previousData = null
+module.exports = (data, date) => {
+  if (data.indexOf('RAW:') == 0) {
+    data = data.replace('RAW:', '')
+    try {
+      data = data.split(',')
+      met = +data[0]
+      freeMemory = parseInt(data[1], 10)
+      battery = data[2] ? parseFloat(data[2]) : null
+      state = data[3].replace(/\n|\r/g, '')
+      agl = data[4] ? parseFloat(data[4]) : null
+      accX = data[5] ? parseFloat(data[5]) : null
+      accY = data[6] ? parseFloat(data[6]) : null
+      accZ = data[7] ? parseFloat(data[7]) : null
+      gyroX = data[8] ? parseFloat(data[8]) : null
+      gyroY = data[9] ? parseFloat(data[9]) : null
+      gyroZ = data[10] ? parseFloat(data[10]) : null
+
+      verticalVelocity = null
+      if (agl != null && previousData && previousData[4]) {
+        dt = met - previousData[0]
+        scale = 1000 / dt
+        diff = agl - parseFloat(previousData[4])
+        verticalVelocity = diff * scale // meters per second
+      }
+
+      connection.query('INSERT INTO raw SET ?', {
+        met, free_memory: freeMemory, battery, state,
+        ground_level: null, agl, altitude: null, pressure: null,
+        acc_x: accX, acc_y: accY, acc_z: accZ,
+        gyro_x: gyroX, gyro_y: gyroY, gyro_z: gyroZ,
+        vertical_velocity: verticalVelocity,
+        created_at: date || new Date()
+      }, (err) => {
+        if (err) console.log(err)
+      })
+    } catch (e) {
+      console.log(e)
+    }
+
+    previousData = data
+  }
+}
--- ground_station/index.js
+++ ground_station/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const WebSocket = require('ws');
 const { EventEmitter } = require('events')
 const app = express()
-const mysql = require('mysql')
+const dbLogger = require('./db-logger')
 
 SERVER_PORT = 3210
 WS_PORT = 8080
@@ -12,15 +12,6 @@ WS_PORT = 8080
 if (!process.env.PORT_PATH) {
   throw new Error('Provide PORT_PATH in env.')
 }
-
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : '',
-  password : '',
-  database : ''
-});
-
-connection.connect();
 
 const dataEmitter = new EventEmitter();
 const parser = new Readline()
@@ -37,49 +28,7 @@ const COMMANDS = {
   trigger_fts: 6
 }
 
-previousData = null
-dataEmitter.on('data', data => {
-  if (data.indexOf('RAW:') == 0) {
-    data = data.replace('RAW:', '')
-    try {
-      data = data.split(',')
-      met = +data[0]
-      freeMemory = parseInt(data[1], 10)
-      battery = data[2] ? parseFloat(data[2]) : null
-      state = data[3].replace(/\n|\r/g, '')
-      agl = data[4] ? parseFloat(data[4]) : null
-      accX = data[5] ? parseFloat(data[5]) : null
-      accY = data[6] ? parseFloat(data[6]) : null
-      accZ = data[7] ? parseFloat(data[7]) : null
-      gyroX = data[8] ? parseFloat(data[8]) : null
-      gyroY = data[9] ? parseFloat(data[9]) : null
-      gyroZ = data[10] ? parseFloat(data[10]) : null
-
-      verticalVelocity = null
-      if (agl != null && previousData && previousData[4]) {
-        dt = met - previousData[0]
-        scale = 1000 / dt
-        diff = agl - parseFloat(previousData[4])
-        verticalVelocity = diff * scale // meters per second
-      }
-
-      connection.query('INSERT INTO raw SET ?', {
-        met, free_memory: freeMemory, battery, state,
-        ground_level: null, agl, altitude: null, pressure: null,
-        acc_x: accX, acc_y: accY, acc_z: accZ,
-        gyro_x: gyroX, gyro_y: gyroY, gyro_z: gyroZ,
-        vertical_velocity: verticalVelocity,
-        created_at: new Date()
-      }, (err) => {
-        if (err) console.log(err)
-      })
-    } catch (e) {
-      console.log(e)
-    }
-
-    previousData = data
-  }
-})
+dataEmitter.on('data', dbLogger)
 
 app.use(express.static('public'))
 app.listen(SERVER_PORT, () => {
