3411bde9bab0b9173ec92ae7b20f99305e649bd6
Themis Papameletiou <themicp@gmail.com>
2020-08-07 03:04:36 +0300

    Update deployer to include all libs for all sketches

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
 SKETCH=$1
 PORT=$2
 BOARD=$3
-LIBS=~/Documents/Arduino/libraries/$SKETCH
+FLIGHT_COMPUTER_LIBS=~/Documents/Arduino/libraries/flight-computer
+GROUND_RECEIVER_LIBS=~/Documents/Arduino/libraries/ground-receiver
 
-if [ ! -d "$LIBS" ]; then
-  mkdir "$LIBS"
+if [ ! -d "$FLIGHT_COMPUTER_LIBS" ]; then
+  mkdir "$FLIGHT_COMPUTER_LIBS"
 else
-  rm -rf ~/Documents/Arduino/libraries/$SKETCH
-  mkdir "$LIBS"
+  rm -rf "$FLIGHT_COMPUTER_LIBS"
+  mkdir "$FLIGHT_COMPUTER_LIBS"
 fi
-cp $SKETCH/libraries/*.{h,cpp} ~/Documents/Arduino/libraries/$SKETCH
-cp -r $SKETCH/libraries/external/ ~/Documents/Arduino/libraries/
+
+if [ ! -d "$GROUND_RECEIVER_LIBS" ]; then
+  mkdir "$GROUND_RECEIVER_LIBS"
+else
+  rm -rf "$GROUND_RECEIVER_LIBS"
+  mkdir "$GROUND_RECEIVER_LIBS"
+fi
+
+cp flight-computer/libraries/*.{h,cpp} "$FLIGHT_COMPUTER_LIBS"
+cp ground-computer/libraries/*.{h,cpp} "$GROUND_RECEIVER_LIBS"
+cp -r flight-computer/libraries/external/ ~/Documents/Arduino/libraries/
+cp -r ground-receiver/libraries/external/ ~/Documents/Arduino/libraries/
 
 arduino-cli compile -b $BOARD $SKETCH && arduino-cli upload -p $PORT -b $BOARD $SKETCH
